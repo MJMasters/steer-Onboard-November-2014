8d65d0ae94b3df2c9f56668c4120382b16c97f13
Nicola <Martin@Martins-MacBook-Air-2.local>
2014-11-14 12:40:42 +0000

    Added Seed data to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+	Room.create([
+		{price_in_pence: 30000,
+		name:"Martin's House", 
+		description: "The Coolest House in the World",
+		address: "27 Salwey Road, Botley, Southampton",
+		latitude: 50.908104,
+		longitude:-1.290699
+		phone_no: "0044 1489 799378", 
+		no_of_rooms: 4,
+		max_no_guests: 8,
+		user_id: 1
+		}
+		{price_in_pence: 50000,
+		name:"Bethany's House", 
+		description: "Next to the Coolest House in the World",
+		address: "25 Salwey Road, Botley, Southampton",
+		latitude: 50.908104,
+		longitude: -1.290699,
+		phone_no: "0044 1489 799377, 
+		no_of_rooms: 6,
+		max_no_guests: 12,
+		user_id: 2}
+	])
